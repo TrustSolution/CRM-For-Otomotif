c6c3bd38a87d7226e3d89abb948106b1bc3398d2
Achmad Zulkarnain Al Jufri <achmad@ts.co.id>
2016-09-18 01:28:26 +0700

    Add files via upload

---
+++ export2excel.php
@@ -0,0 +1,8 @@
+<?php header("Content-type: application/x-msdownload"); 
+header("Content-Disposition: attachment; filename=tbl_customer.xls"); 
+echo "Cust Id	Cust Kelompok	Cust Nama	Cust Kelamin	Cust Tgllahir	Agama Nama	Cust Alamat	Cust Kec	Cust Kota	Cust Npwp	Cust Pekerjaan	Cust Tempatkerja	Cust Bidangusaha	Cust Jabatan	Cust Notelprumah	Cust Notelpkantor	Cust Nofax	Cust Noponsel	Cust Noponsel2	Cust Email	Cust Aktif	
+\"9\"	\"Pribadi\"	\"Zaenal Muttaqin\"	\"L\"	\"06/09/1955\"	\"Kristen\"	\"Alamat\"	\"Kecamatan\"	\"Surabaya\"	\"123321\"	\"Programmer\"	\"TRISULA\"	\"IT\"	\"Manager Produksi\"	\"0318788887\"	\"03131313\"		\"0811212122\"		\"zaenal@gmail.com\"	\"Y\"
+\"16\"	\"Pribadi\"	\"songbee\"	\"L\"	\"07/02/2006\"	\"Islam\"	\"alamamt\"	\"kececema\"	\"kotaa\"	\"964646463\"	\"sfdfdsf\"	\"sdfsdfsdf\"	\"sdfdsfsdf\"	\"sdfdsfsdf\"	\"0643323\"	\"3123232\"		\"0616126216\"	\"01616161\"		\"Y\"
+\"17\"	\"Pribadi\"	\"tesssssssssss\"	\"L\"	\"06/09/2006\"	\"Islam\"	\"sdfasf\"	\"sadfsdf\"	\"afsadfsa\"	\"234232323\"	\"sdfdsfsd\"	\"fdsfsdf\"	\"fsdfsdfsd\"	\"fsdfsdfs\"	\"332423\"	\"32423232\"		\"23423\"	\"23423\"		\"Y\"
+\"28\"	\"Perusahaan\"	\"asdfasdf\"	\"L\"		\"-\"	\"sdfdsf\"	\"sdf\"	\"sdf\"											\"dsfsd@sadfasdf.sdf\"
+";?>
